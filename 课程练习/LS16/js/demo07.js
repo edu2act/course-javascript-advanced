0d8c27a2cf9adddaf87f4083ec2dbab0f0cf77e4
Qi Le <qile@onest.net>
2018-04-24 16:51:08 +0800

    V1.16.1 调整16、17课程的顺序（4月24号）

--- 课程练习/LS16/js/demo07.js
+++ 课程练习/LS16/js/demo07.js
@@ -1,12 +1,63 @@
 /**
  * Created by qile on 2017/8/14.
  */
-//通过Object.create静态方法创建的对象的原型共享问题
-var superObj = {
-    x:1,
-    y:2
+// Part1 构造函数中函数嵌套 this缺陷
+function Point(x,y) {
+    this.x = x;
+    this.y = y;
+    this.moveXY = function (x,y) {
+        function moveX(x) {
+            this.x+=x;
+        }
+        function moveY(y) {
+            this.y+=y;
+        }
+        moveX(x);
+        moveY(y);
+    }
+}
+var p = new Point(2,3);
+p.moveXY(1,1);
+console.log(p);//输出为Point{x:2,y:3}没有移动
+
+
+
+// Part2 构造函数中函数嵌套 this缺陷 解决方案
+function Point(x,y) {
+    this.x = x;
+    this.y = y;
+    this.moveXY = function (x,y) {
+        var that = this;//此处that为实例化出来的p对象
+        function moveX(x) {
+            that.x+=x;//this改为that
+        }
+        function moveY(y) {
+            that.y+=y;//this改为that
+        }
+        moveX(x);
+        moveY(y);
+    }
+}
+var p = new Point(2,3);
+p.moveXY(1,1);
+console.log(p);//输出为Point{x:3,y:4}，移动了(1,1)
+
+
+//思考：下述代码
+var obj = {
+    name:"obj",
+    x:23,
+    test:function(){
+        console.log(this.x,this);
+    }
 };
-var subObj_First = Object.create(superObj);
-var subObj_Second = Object.create(superObj);
-subObj_First.__proto__.x = 5;
-console.log(subObj_Second.x);
+var fun1 = function () {
+    return function fun2() {
+        return this.x;//若改为 return this;
+    }
+};
+obj.fun3 = fun1;
+obj.fun4 = fun1();
+console.log(obj.fun3());//输出什么
+console.log(obj.fun3()());//输出什么
+console.log(obj.fun4());//输出什么
